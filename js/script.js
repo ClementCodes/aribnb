0d5b860d104870bc5cbbf6007ec101a8ecdc4466
clementcode <mrdominiqueclement@gmail.com>
2021-12-04 15:54:00 +0100

    button-color

--- js/script.js
+++ js/script.js
@@ -1,55 +1,22 @@
 fetch('activities.json')
     .then(response => response.json())
     .then(data => {
-        // console.log(data)
-        // const col = document.querySelector('.col-md-6')
-        // console.log(col)
-        // col.style.backgroundColor = "blue"
-        // const colList = document.querySelectorAll('.col-md-6')
-        // console.log(colList)
-        // colList[1].style.backgroundColor = "orange"
-        // colList.forEach(element => {
-        //     element.style.backgroundColor = "orange"
-        // })
-
-        // const div = document.createElement('div')
-        // div.classList.add('container')
-        // console.log(div)
-        // const body = document.body
-        // console.log(body)
-        // body.append(div)
-        // body.prepend(div)
-
         data.map(element => {
-          
-            // create object html h3
-         const div = document.createElement('h3')
-            // insert value title
-
-        div.classList.add("title")
-            // insert object h3 into .activities
-let acti = 
-            div.append(title)
-            
-            return hello
-
-
-
-
-            
-
-
-           
-
-
-
-
-
-
+           let left = document.getElementById('left')
+let  integration =left.insertAdjacentHTML('afterbegin',`<div class="monument"><img src="${element.imageSrc}" alt="memorial Bordeaux" /> </div><div class="description"><h1>${element.title}</h1><h3>${element.address}, ${element.postalCode}</h3><p>${element.description}</p><div></div><span><button>${element.votes} Voter  <span>  &hearts;</span>   </button></span></div>`)
+ 
         });
-
-          
-
-
     })
     .catch(error => console.log(error))
+    fetch('activities.json')
+    .then(response => response.json())
+    .then(data => {
+   
+
+    })
+    
+    
+    
+
+  
+
